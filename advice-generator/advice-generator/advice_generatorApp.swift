b74c4b065742f39c78020b9c7f82191b5e23d33f
Gabriel Santiago <gaaasts@gmail.com>
2022-08-22 08:59:12 -0300

    first commit

---
+++ advice-generator/advice-generator/advice_generatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  advice_generatorApp.swift
+//  advice-generator
+//
+//  Created by Gabriel Santiago on 22/08/22.
+//
+
+import SwiftUI
+
+@main
+struct advice_generatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
